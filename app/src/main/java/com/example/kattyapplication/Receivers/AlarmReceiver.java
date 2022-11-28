5765e29de221ce0e768599ac132cac0c6c91d22d
TrongTre07 <trongtruth2003@gmail.com>
2022-11-28 10:37:01 +0700

    update referances content

--- app/src/main/java/com/example/kattyapplication/Receivers/AlarmReceiver.java
+++ app/src/main/java/com/example/kattyapplication/Receivers/AlarmReceiver.java
@@ -1,5 +1,6 @@
 package com.example.kattyapplication.Receivers;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.content.Intent;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.example.kattyapplication.MainActivity;
 import com.example.kattyapplication.R;
 import com.example.kattyapplication.fragment.RemindFragment;
 
@@ -15,6 +17,11 @@ public class AlarmReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
 
         String content = intent.getStringExtra("content");
+
+        Intent i = new Intent(context, MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 1, i, PendingIntent.FLAG_IMMUTABLE);
+
 
 
 //        Resources res = content.getResources();
@@ -25,6 +32,8 @@ public class AlarmReceiver extends BroadcastReceiver {
                 .setSmallIcon(R.mipmap.katty_logo)
                 .setContentTitle("Nhắc Nhở")
                 .setContentText(content)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
                 .setDefaults(NotificationCompat.DEFAULT_ALL)
                 .setPriority(NotificationCompat.PRIORITY_HIGH);
 
